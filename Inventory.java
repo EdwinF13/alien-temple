9932ebcb3be21d7585ad43f93c630ae0c10944a1
Zayan Aly <114845721+zayanaly@users.noreply.github.com>
2023-11-15 12:15:50 -0600

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -7,3 +7,37 @@ class Inventory
     ArrayList<Item> hotBar = new ArrayList<>();
     ArrayList<Item> extraStorage = new ArrayList<>();
 }
+
+/////////////////////////////////////////////////////////////
+
+package Alien_Temple;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventory {
+    private List<Item> items;
+
+    public Inventory() {
+        items = new ArrayList<>();
+    }
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public boolean removeItem(Item item) {
+        return items.remove(item);
+    }
+
+    public void displayInventory() {
+        if (items.isEmpty()) {
+            System.out.println("Your inventory is empty.");
+        } else {
+            System.out.println("Inventory:");
+            for (Item item : items) {
+                item.printItemDetails();
+            }
+        }
+    }
+}
