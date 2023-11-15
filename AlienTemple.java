045809540ea01894a69576a97dc93d397f4b33d1
Zayan Aly <114845721+zayanaly@users.noreply.github.com>
2023-11-15 12:28:53 -0600

    Update AlienTemple.java

--- AlienTemple.java
+++ AlienTemple.java
@@ -93,3 +93,105 @@ public class AlienTemple
         }
     }              
 }
+
+/////////////////////////////////////////////////////////////////////////////////
+
+package Alien_Temple;
+
+import java.util.Scanner;
+
+public class AlienTemple {
+    private Researcher player;
+    private Map gameMap;
+
+    public AlienTemple() {
+        this.player = new Researcher("Space Adventurer", 100);
+        this.gameMap = new Map(player);
+        // Initialize the game map with rooms, items, etc.
+    }
+
+    public void startGame() {
+        // Assuming introParagraph is a class member now
+        System.out.println(introParagraph);
+        gameMap.navigate(0); // Start the player in the first room
+        gameLoop();
+    }
+
+    private void gameLoop() {
+        Scanner scanner = new Scanner(System.in);
+        boolean gameRunning = true;
+
+        while (gameRunning) {
+            System.out.println("\nChoose an action: [navigate] [inventory] [quit]");
+            String action = scanner.nextLine().toLowerCase();
+
+            switch (action) {
+                case "navigate":
+                    handleNavigation(scanner);
+                    break;
+                case "inventory":
+                    player.getInventory().displayInventory();
+                    break;
+                case "quit":
+                    gameRunning = false;
+                    System.out.println("Exiting game...");
+                    break;
+                default:
+                    System.out.println("Invalid action. Please try again.");
+                    break;
+            }
+        }
+
+        scanner.close();
+    }
+
+    private void handleNavigation(Scanner scanner) {
+        System.out.println("Enter the room number to navigate:");
+        int roomIndex;
+        try {
+            roomIndex = Integer.parseInt(scanner.nextLine()) - 1; // Adjust for zero-based indexing
+            gameMap.navigate(roomIndex);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid room number. Please enter a valid number.");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            AlienTemple game = new AlienTemple();
+            Scanner sc = new Scanner(System.in);
+
+            // Intro paragraph
+            String introParagraph = "ALIEN TEMPLE\n\n" +
+                "The year is 2739. You are a space adventurer,\n" +
+                "answering the distress call of a research outpost\n" +
+                "on a mysterious planet. When you arrive at the\n" +
+                "outpost, you find it abandoned; the researchers\n" +
+                "were apparently investigating a nearby alien temple,\n" +
+                "but one day it seems they never returned from the\n" +
+                "temple, and yet strangely their distress beacon was\n" +
+                "activated. Seeing no other choice, you enter the\n" +
+                "temple hoping to rescue the missing researchers....\n";
+            
+            System.out.println("Do you want to start a new game or load an existing game? [new/load]");
+            String choice = sc.nextLine().toLowerCase();
+
+            switch (choice) {
+                case "new":
+                    game.startGame();
+                    break;
+                case "load":
+                    // Load game functionality (to be implemented)
+                    break;
+                default:
+                    System.out.println("Exiting game...");
+                    break;
+            }
+
+            sc.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
