a61fdc898da845009c83854f122e0f66d2d6b377
Zayan Aly <114845721+zayanaly@users.noreply.github.com>
2023-11-15 12:12:40 -0600

    Update Weapon.java

--- Weapon.java
+++ Weapon.java
@@ -27,3 +27,28 @@ class Weapon
         return dmgOutput;
     }
 }
+
+///////////////////////////////////////////////////////////
+
+package Alien_Temple;
+
+public class Weapon extends Item {
+    private String weaponStats;
+    private int damageOutput;
+
+    public Weapon(String name, String description, int damageOutput, String weaponStats) {
+        super(name, description);  // Call the constructor of the Item class
+        this.damageOutput = damageOutput;
+        this.weaponStats = weaponStats;
+    }
+
+    public String getWeaponStats() {
+        return weaponStats;
+    }
+
+    public int getDamageOutput() {
+        return damageOutput;
+    }
+
+    // You can add more methods specific to weapons, such as useWeapon, enhanceWeapon, etc.
+}
