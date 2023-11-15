51d5f6b711326e900700db1e4f52b0e501559d0e
Zayan Aly <114845721+zayanaly@users.noreply.github.com>
2023-11-15 12:15:02 -0600

    Update Monster.java

--- Monster.java
+++ Monster.java
@@ -35,3 +35,54 @@ class Monster
         System.out.println("DMG Output: " + monsterDamageOutput);
     }
 }
+
+/////////////////////////////////////////////////////////////////////////////
+
+package Alien_Temple;
+
+public class Monster {
+    private String species;
+    private String description;
+    private int health;
+    private int damage;
+
+    public Monster(String species, String description, int health, int damage) {
+        this.species = species;
+        this.description = description;
+        this.health = health;
+        this.damage = damage;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void attack(Player player) {
+        System.out.println("The " + species + " attacks!");
+        player.decreaseHealth(damage);
+    }
+
+    public void takeDamage(int amount) {
+        health -= amount;
+        if (health <= 0) {
+            System.out.println(species + " has been defeated!");
+        }
+    }
+
+    public void displayMonsterDetails() {
+        System.out.println("Encountered a " + species);
+        System.out.println(description);
+    }
+}
