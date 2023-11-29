a27861f3b52bcc9c20c8392e7aacb89cb551ad47
EdwinF13 <150871236+EdwinF13@users.noreply.github.com>
2023-11-29 09:31:02 -0600

    Update AlienTemple.java

--- AlienTemple.java
+++ AlienTemple.java
@@ -194,4 +194,341 @@ public class AlienTemple {
         }
     }
 }
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+package Alien_Temple;
+
+import java.util.Scanner;
+
+
+
+public class AlienTemple {
+    
+    int choice;
+    int playerHP;
+    int monsterHP;
+    String playerName;
+    String playerWeapon;
+    int NorthKey;
+    int obeliskKey;
+    
+    Scanner myScanner = new Scanner(System.in);
+
+public static void main(String[] args){
+        AlienTemple game;
+        game = new AlienTemple();
+        game.playerSetUp();
+        game.Room();
+    }
+  
+public void playerSetUp(){
+  
+  playerHP = 300;
+  monsterHP = 500;
+ 
+  String playerWeapon;
+  playerWeapon ="Barefist";
+  
+
+  System.out.println("Your Hp: " +playerHP);
+  System.out.println("Your weapon: " +playerWeapon);
+
+  String playerName;
+
+  
+
+  System.out.println("Please enter your name: \n");
+  playerName = myScanner.nextLine();
+
+  System.out.println("Hello " + playerName + ", let us begin!!\n");
+  System.out.println("");
+  String introParagraph = "ALIEN TEMPLE\n\n"
+  + "The year is 2739. You are a space adventurer,\n"
+  + "answering the distress call of a research outpost\n"
+  + "on a mysterious planet. When you arrive at the\n"
+  + "outpost, you find it abandoned: the researchers\n"
+  + "were apparently investigating a nearby alien temple,\n"
+  + "but one day it seems they never returned from the\n"
+  + "temple, and yet strangely their distress beacon was\n"
+  + "activated. Seeing no other choice, you enter the\n"
+  + "temple hoping to rescue the missing researchers....\n";
+System.out.println(introParagraph);
+
+
+
+}
+
+
+public void Room() {
+
+    System.out.println("\n--------------------------------------------\n");
+    System.out.println(" You have entered the Main room you see different symbols of what appears to be different shapes and notice the place is eerie and silent ");
+    System.out.println(" You find a dead body could this be one of the researchers you ask?");
+    System.out.println("You approach the body");
+    
+        
+        System.out.println("You found one of the researchers his body seems to have multiple scratches.");
+        System.out.println(" You hear rumbling.............");
+        System.out.println(" The door has closed behind you, you try to open it and it is locked");
+        System.out.println("You see three different rooms which one do you want to enter?");
+        System.out.println("\n-----------------------------------\n");
+        System.out.println("1: Go to West Room");
+        System.out.println("2:Go to North Room ");
+        System.out.println("3: Go to East Room");
+         System.out.println("4: Main door out");
+        choice = myScanner.nextInt();
+      if (choice == 1)
+      {
+        WestRoom();
+    }
+
+      if (choice == 2){
+        
+            NorthRoom();
+        }
+        
+    
+
+    if (choice == 3){
+        EastRoom();
+    
+    }
+
+    if (choice == 4){
+        if(obeliskKey == 1){
+            end();
+        }else{
+            System.out.println("Door is locked");
+            MainRoom();
+                }
+            }
+        }
+    
+
+
+    
+
+public void WestRoom(){
+    System.out.println("\n--------------------------------------------\n");
+   System.out.println(" You have entered a strange room you find what appears to be a stream and you are thirsty\n");
+   System.out.println(" What do you want to do ?\n");
+    System.out.println("1:Drink the water ");
+    System.out.println("2:leave it alone");
+    
+     
+    choice = myScanner.nextInt();
+    if(choice == 1 ){
+        if (playerHP < 400){
+         System.out.println("\n------------------------------------------------------------------\n");
+         System.out.println("Your HP increased.\n");
+         playerHP = playerHP + 10;
+         System.out.println("Your HP: " + playerHP);
+         
+         MainRoom();
+        }
+      else {
+        System.out.println("You are at max Health!");
+        
+    }
+    MainRoom();
+} else if (choice == 2){
+    MainRoom();
+}
+}
+public void NorthRoom(){
+    System.out.println("\n------------------------------------------------------------------\n");
+    System.out.println("The door closes behind you\n");
+    System.out.println("You encounter some form of alien lifeform that is running towards you\n");
+    System.out.println("What do you want to do?\n");
+    System.out.println("1: Run to the door and try to open it");
+    System.out.println("2: Fight ");
+    
+
+    choice = myScanner.nextInt();
+
+    if (choice == 1) {
+        System.out.println("Door locked!");
+        NorthRoom();
+        
+    } else if (choice == 2) {
+        fight();
+    } else {
+        NorthRoom();
+    }
+}
+
+public void MainRoom(){
+    System.out.println("\n--------------------------------------------\n");
+    System.out.println(" Main room\n");
+    System.out.println("1:Go to West Room");
+    System.out.println("2:Go to North room ");
+    System.out.println("3:Go to East Room");
+    System.out.println("4:Get out of temple");
+    
+    choice = myScanner.nextInt();
+
+    if (choice == 1){
+        WestRoom();
+    }
+
+     if (choice == 2){
+
+            NorthRoom();
+        }
+       
+    
+
+    if (choice == 3){
+        EastRoom();
+    }  
+if (choice == 4){
+        if(obeliskKey == 1){
+            end();
+        }else{
+            System.out.println("Door is locked");
+            MainRoom();
+                }
+            }
+
+
+}
+    
+    
+public void EastRoom(){
+    System.out.println("\n--------------------------------------------\n");
+    System.out.println("You find another researcher dead but he carried a Rifle!!");
+    System.out.println("You take the Rifle with you along with a key that he carried ");
+    NorthKey = 1;
+    playerWeapon ="Rifle";
+    System.out.println("Your Weapon:\n " +playerWeapon);
+    System.out.println("\n\n1: Go back to the main road");
+    System.out.println("\n--------------------------------------------\n");
+    choice = myScanner.nextInt();
+
+    if(choice == 1)
+    {
+    
+        MainRoom();
+    } else {
+        MainRoom();
+    }
+    
+   }
+
+    
+
+
+
+
+
+
+  
+    
+
+
+
+public void fight() {
+    
+    System.out.println("\n------------------------------------------------------------------\n");
+    System.out.println("Your HP: " + playerHP);
+    System.out.println("Monster HP: " + monsterHP);
+    System.out.println("\n1:Attack");
+    System.out.println("\n------------------------------------------------------------------\n");
+
+    choice = myScanner.nextInt();
+
+    
+    if (choice == 1) {
+    
+        attack();
+    }
+        else {
+           fight();
+        }
+    }
+
+
+
+
+public void attack() {
+    int playerDamage = 10;
+    if (playerWeapon == null) {
+        // Initialize playerWeapon to a default value if it is null
+        playerWeapon = "Barefist"; // Or any other default weapon
+    }
+    if (playerWeapon.equals("Rifle")) {
+        playerDamage = new java.util.Random().nextInt(90);
+    } else if(playerWeapon.equals("Barefist")){
+        playerDamage = new java.util.Random().nextInt(20);
+    }
+    
+
+    System.out.println("You attacked the monster and gave " + playerDamage + " damage!");
+
+    monsterHP = monsterHP - playerDamage;
+
+    System.out.println("Monster HP: " + monsterHP);
+
+    if (monsterHP < 1) {
+        win();
+    } 
+    else if (monsterHP > 0) {
+        int monsterDmg = 15;
+
+        monsterDmg= new java.util.Random().nextInt(55);
+        System.out.println("The monster attacked you and gave " + monsterDmg + " damage!");
+
+        playerHP = playerHP - monsterDmg;
+
+        System.out.println("Player HP: " + playerHP);
+
+        if (playerHP < 1) {
+            dead();
+        } 
+
+         else if (playerHP > 0){
+           
+             fight();
+            }
+        }
+    }
+
+    
+
+
+
+
+public void dead() {
+    System.out.println("\n------------------------------------------------------------------\n");
+    System.out.println("YOU DIED!!!");
+    System.out.println("\n\nGAME OVER");
+    System.out.println("\n------------------------------------------------------------------\n");
+    System.exit(0);
+}
+
+
+public void win() {
+		System.out.println("\n------------------------------------------------------------------\n");
+		System.out.println("You killed the Alien!");
+		System.out.println("The monster dropped a obelisk key!");
+		System.out.println("You obtained an obelisk key!\n\n");
+		System.out.println("1: You use it to open the door!");
+		System.out.println("\n------------------------------------------------------------------\n");
+
+		
+        obeliskKey = 1;
+		choice = myScanner.nextInt();
+		if (choice == 1) {
+			MainRoom();
+		}
+
+	}
+
+public void end() {
+		System.out.println("\n------------------------------------------------------------------\n");
+		System.out.println("You avenged the fellow Researchers and live for another Adventure!!!");
+		System.out.println("\n\n  THE END                 ");
+		System.out.println("\n------------------------------------------------------------------\n");
+	}
+}
+
 
